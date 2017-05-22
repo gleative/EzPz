a6fe215a7b656005c195b606af671fcaa2bbe97e
Marius Steller Imingen <marius.steller@gmail.com>
2017-05-22 14:25:30 +1000

    Somethings

--- myQuack.py
+++ myQuack.py
@@ -19,7 +19,7 @@ def my_team():
     '''
     Return the list of the team members of this assignment submission as a list
     of triplet of the form (student_number, first_name, last_name)
-    
+
     '''
 #    return [ (1234567, 'Ada', 'Lovelace'), (1234568, 'Grace', 'Hopper'), (1234569, 'Eva', 'Tardos') ]
     return [ (9890394, 'Vanessa', 'Gutierrez'), (9884050, 'Glenn', 'Christensen'), (9884076, 'Marius', 'Imingen') ]
@@ -28,13 +28,13 @@ def my_team():
 
 
 def prepare_dataset(dataset_path):
-    '''  
-    Read a comma separated text file where 
-	- the first field is a ID number 
+    '''
+    Read a comma separated text file where
+	- the first field is a ID number
 	- the second field is a class label 'B' or 'M'
 	- the remaining fields are real-valued
 
-    Return two numpy arrays X and y where 
+    Return two numpy arrays X and y where
 	- X is two dimensional. X[i,:] is the ith example
 	- y is one dimensional. y[i] is the class label of X[i,:]
           y[i] should be set to 1 for 'M', and 0 for 'B'
@@ -44,71 +44,71 @@ def prepare_dataset(dataset_path):
     @return
 	X,y
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def build_NB_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Naive Bayes classifier based on the training set X_training, y_training.
 
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
 	clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def build_DT_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Decision Tree classifier based on the training set X_training, y_training.
 
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
 	clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def build_NN_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Nearrest Neighbours classifier based on the training set X_training, y_training.
 
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
 	clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def build_SVM_classifier(X_training, y_training):
-    '''  
+    '''
     Build a Support Vector Machine classifier based on the training set X_training, y_training.
 
-    @param 
+    @param
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
 
     @return
 	clf : the classifier built in this function
     '''
-    ##         "INSERT YOUR CODE HERE"    
+    ##         "INSERT YOUR CODE HERE"
     raise NotImplementedError()
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -116,6 +116,5 @@ def build_SVM_classifier(X_training, y_training):
 if __name__ == "__main__":
     pass
     # call your functions here
-    
 
 
